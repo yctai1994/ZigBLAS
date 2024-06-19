1e85e18b7f61ac7067bd8b31f577e3dd99d48c23
yctai1994 <yctai.git@gmail.com>
2024-06-20 02:54:34 +0900

    fix

--- src/BLAS/level3/dtrsm.zig
+++ src/BLAS/level3/dtrsm.zig
@@ -13,11 +13,10 @@ pub fn dtrsm(
     ldb: usize,
 ) void {
     const zero: T = 0;
-    const one: T = 1;
+    // const one: T = 1;
 
-    const lside: bool = side == 'L';
-    const upper: bool = uplo == 'U';
-    const nounit: bool = diag == 'N';
+    // const upper: bool = uplo == 'U';
+    // const nounit: bool = diag == 'N';
 
     switch (side) {
         'L', 'l' => if (lda < @max(1, m)) unreachable,
@@ -62,172 +61,227 @@ pub fn dtrsm(
     }
 
     // Start the operations.
-    var temp: T = undefined;
+    const temp: T = undefined;
+    _ = temp;
 
-    if (lside) {
-        if (transa == 'N') {
+    switch (side) {
+        'L', 'l' => {
+            switch (transa) {
+                'N', 'n' => {
 
-            // Form  B := alpha*inv( A )*B.
+                    // Form  B := alpha⋅(A)⁻¹⋅B.
 
-            if (upper) {
-                for (1..n) |j| {
-                    if (alpha != one) {
-                        for (1..m) |i| {
-                            B[i][j] = alpha * B[i][j];
-                        }
+                    switch (uplo) {
+                        'U', 'u' => {},
+                        'L', 'l' => {},
+                        else => @compileError(""),
                     }
-                    for (m..1) |k| {
-                        if (B[k][j] != zero) {
-                            if (nounit) B[k][j] = B[k][j] / A[k][k];
-                            for (1..k - 1) |i| {
-                                B[i][j] = B[i][j] - B[k][j] * A[i][k];
-                            }
-                        }
+                },
+                'T', 't' => {
+
+                    // Form  B := alpha⋅(Aᵀ)⁻¹⋅B.
+
+                    switch (uplo) {
+                        'U', 'u' => {},
+                        'L', 'l' => {},
+                        else => @compileError(""),
                     }
-                }
-            } else {
-                for (1..n) |j| {
-                    if (alpha != one) {
-                        for (1..m) |i| {
-                            B[i][j] = alpha * B[i][j];
-                        }
-                    }
-                    for (1..m) |k| {
-                        if (B[k][j] != zero) {
-                            if (nounit) B[k][j] = B[k][j] / A[k][k];
-                            for (k + 1..m) |i| {
-                                B[i][j] = B[i][j] - B[k][j] * A[i][k];
-                            }
-                        }
-                    }
-                }
+                },
+                else => @compileError(""),
             }
-        } else {
+        },
+        'R', 'r' => {
+            switch (transa) {
+                'N', 'n' => {
 
-            // Form  B := alpha*inv( A**T )*B.
+                    // Form  B := alpha⋅B⋅(A)⁻¹.
 
-            if (upper) {
-                for (1..n) |j| {
-                    for (1..m) |i| {
-                        temp = alpha * B[i][j];
-                        for (1..i - 1) |k| {
-                            temp = temp - A[k][i] * B[k][j];
-                        }
-                        if (nounit) temp = temp / A[i][i];
-                        B[i][j] = temp;
+                    switch (uplo) {
+                        'U', 'u' => {},
+                        'L', 'l' => {},
+                        else => @compileError(""),
                     }
-                }
-            } else {
-                for (1..n) |j| {
-                    for (m..1) |i| {
-                        temp = alpha * B[i][j];
-                        for (i + 1..m) |k| {
-                            temp = temp - A[k][i] * B[k][j];
-                        }
-                        if (nounit) temp = temp / A[i][i];
-                        B[i][j] = temp;
+                },
+                'T', 't' => {
+
+                    // Form  B := alpha⋅B⋅(Aᵀ)⁻¹.
+
+                    switch (uplo) {
+                        'U', 'u' => {},
+                        'L', 'l' => {},
+                        else => @compileError(""),
                     }
-                }
+                },
+                else => @compileError(""),
             }
-        }
-    } else {
-        if (transa == 'N') {
-
-            // Form  B := alpha*B*inv( A ).
-
-            if (upper) {
-                for (1..n) |j| {
-                    if (alpha != one) {
-                        for (1..m) |i| {
-                            B[i][j] = alpha * B[i][j];
-                        }
-                    }
-                    for (1..j - 1) |k| {
-                        if (A[k][j] != zero) {
-                            for (1..m) |i| {
-                                B[i][j] = B[i][j] - A[k][j] * B[i][k];
-                            }
-                        }
-                    }
-                    if (nounit) {
-                        temp = one / A[j][j];
-                        for (1..m) |i| {
-                            B[i][j] = temp * B[i][j];
-                        }
-                    }
-                }
-            } else {
-                for (n..1) |j| {
-                    if (alpha != one) {
-                        for (1..m) |i| {
-                            B[i][j] = alpha * B[i][j];
-                        }
-                    }
-                    for (j + 1..n) |k| {
-                        if (A[k][j] != zero) {
-                            for (1..m) |i| {
-                                B[i][j] = B[i][j] - A[k][j] * B[i][k];
-                            }
-                        }
-                    }
-                    if (nounit) {
-                        temp = one / A[j][j];
-                        for (1..m) |i| {
-                            B[i][j] = temp * B[i][j];
-                        }
-                    }
-                }
-            }
-        } else {
-
-            // Form  B := alpha*B*inv( A**T ).
-
-            if (upper) {
-                for (n..1) |k| {
-                    if (nounit) {
-                        temp = one / A[k][k];
-                        for (1..m) |i| {
-                            B[i][k] = temp * B[i][k];
-                        }
-                    }
-                    for (1..k - 1) |j| {
-                        if (A[j][k] != zero) {
-                            temp = A[j][k];
-                            for (1..m) |i| {
-                                B[i][j] = B[i][j] - temp * B[i][k];
-                            }
-                        }
-                    }
-                    if (alpha != one) {
-                        for (1..m) |i| {
-                            B[i][k] = alpha * B[i][k];
-                        }
-                    }
-                }
-            } else {
-                for (1..n) |k| {
-                    if (nounit) {
-                        temp = one / A[k][k];
-                        for (1..m) |i| {
-                            B[i][k] = temp * B[i][k];
-                        }
-                    }
-                    for (k + 1..n) |j| {
-                        if (A[j][k] != zero) {
-                            temp = A[j][k];
-                            for (1..m) |i| {
-                                B[i][j] = B[i][j] - temp * B[i][k];
-                            }
-                        }
-                    }
-                    if (alpha != one) {
-                        for (1..m) |i| {
-                            B[i][k] = alpha * B[i][k];
-                        }
-                    }
-                }
-            }
-        }
+        },
+        else => @compileError(""),
     }
+    // if (lside) {
+    //     if (transa == 'N') {
+
+    //         // Form  B := alpha⋅(A)⁻¹⋅B.
+
+    //         if (upper) {
+    //             for (1..n) |j| {
+    //                 if (alpha != one) {
+    //                     for (1..m) |i| {
+    //                         B[i][j] = alpha * B[i][j];
+    //                     }
+    //                 }
+    //                 for (m..1) |k| {
+    //                     if (B[k][j] != zero) {
+    //                         if (nounit) B[k][j] = B[k][j] / A[k][k];
+    //                         for (1..k - 1) |i| {
+    //                             B[i][j] = B[i][j] - B[k][j] * A[i][k];
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             for (1..n) |j| {
+    //                 if (alpha != one) {
+    //                     for (1..m) |i| {
+    //                         B[i][j] = alpha * B[i][j];
+    //                     }
+    //                 }
+    //                 for (1..m) |k| {
+    //                     if (B[k][j] != zero) {
+    //                         if (nounit) B[k][j] = B[k][j] / A[k][k];
+    //                         for (k + 1..m) |i| {
+    //                             B[i][j] = B[i][j] - B[k][j] * A[i][k];
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     } else {
+
+    //         // Form  B := alpha⋅(Aᵀ)⁻¹⋅B.
+
+    //         if (upper) {
+    //             for (1..n) |j| {
+    //                 for (1..m) |i| {
+    //                     temp = alpha * B[i][j];
+    //                     for (1..i - 1) |k| {
+    //                         temp = temp - A[k][i] * B[k][j];
+    //                     }
+    //                     if (nounit) temp = temp / A[i][i];
+    //                     B[i][j] = temp;
+    //                 }
+    //             }
+    //         } else {
+    //             for (1..n) |j| {
+    //                 for (m..1) |i| {
+    //                     temp = alpha * B[i][j];
+    //                     for (i + 1..m) |k| {
+    //                         temp = temp - A[k][i] * B[k][j];
+    //                     }
+    //                     if (nounit) temp = temp / A[i][i];
+    //                     B[i][j] = temp;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // } else {
+    //     if (transa == 'N') {
+
+    //         // Form  B := alpha⋅B⋅(A)⁻¹.
+
+    //         if (upper) {
+    //             for (1..n) |j| {
+    //                 if (alpha != one) {
+    //                     for (1..m) |i| {
+    //                         B[i][j] = alpha * B[i][j];
+    //                     }
+    //                 }
+    //                 for (1..j - 1) |k| {
+    //                     if (A[k][j] != zero) {
+    //                         for (1..m) |i| {
+    //                             B[i][j] = B[i][j] - A[k][j] * B[i][k];
+    //                         }
+    //                     }
+    //                 }
+    //                 if (nounit) {
+    //                     temp = one / A[j][j];
+    //                     for (1..m) |i| {
+    //                         B[i][j] = temp * B[i][j];
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             for (n..1) |j| {
+    //                 if (alpha != one) {
+    //                     for (1..m) |i| {
+    //                         B[i][j] = alpha * B[i][j];
+    //                     }
+    //                 }
+    //                 for (j + 1..n) |k| {
+    //                     if (A[k][j] != zero) {
+    //                         for (1..m) |i| {
+    //                             B[i][j] = B[i][j] - A[k][j] * B[i][k];
+    //                         }
+    //                     }
+    //                 }
+    //                 if (nounit) {
+    //                     temp = one / A[j][j];
+    //                     for (1..m) |i| {
+    //                         B[i][j] = temp * B[i][j];
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     } else {
+
+    //         // Form  B := alpha⋅B⋅(Aᵀ)⁻¹.
+
+    //         if (upper) {
+    //             for (n..1) |k| {
+    //                 if (nounit) {
+    //                     temp = one / A[k][k];
+    //                     for (1..m) |i| {
+    //                         B[i][k] = temp * B[i][k];
+    //                     }
+    //                 }
+    //                 for (1..k - 1) |j| {
+    //                     if (A[j][k] != zero) {
+    //                         temp = A[j][k];
+    //                         for (1..m) |i| {
+    //                             B[i][j] = B[i][j] - temp * B[i][k];
+    //                         }
+    //                     }
+    //                 }
+    //                 if (alpha != one) {
+    //                     for (1..m) |i| {
+    //                         B[i][k] = alpha * B[i][k];
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             for (1..n) |k| {
+    //                 if (nounit) {
+    //                     temp = one / A[k][k];
+    //                     for (1..m) |i| {
+    //                         B[i][k] = temp * B[i][k];
+    //                     }
+    //                 }
+    //                 for (k + 1..n) |j| {
+    //                     if (A[j][k] != zero) {
+    //                         temp = A[j][k];
+    //                         for (1..m) |i| {
+    //                             B[i][j] = B[i][j] - temp * B[i][k];
+    //                         }
+    //                     }
+    //                 }
+    //                 if (alpha != one) {
+    //                     for (1..m) |i| {
+    //                         B[i][k] = alpha * B[i][k];
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    _ = .{ A, B };
     return;
 }
